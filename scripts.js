a98a19a0d8873f536b194168f4a353f7cdd509f5
VoGPro <112944896+VoGPro@users.noreply.github.com>
2022-10-14 21:06:43 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function click1() {
+function click1(event) {
   let f1 = document.getElementsByName("field1");
   let f2 = document.getElementsByName("field2");
   let r = document.getElementById("result");
@@ -9,5 +9,11 @@ function click1() {
   if (isNaN(v1) || isNaN(v2)) alert("Введите корректное значение!");
   else r.innerHTML = v1 * v2;
 
-  return false;
+  event.preventDefault();
 }
+
+window.addEventListener('DOMContentLoaded', function () {
+  console.log("DOM fully loaded and parsed");
+  let b = document.getElementById("myForm");
+  b.addEventListener("submit", click1);
+})
